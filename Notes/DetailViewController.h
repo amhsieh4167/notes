0e735ef3e6f65e479b78a050391383b8ff1259d5
Alex Hsieh <amhsieh@gmail.com>
2012-11-02 18:51:31 -0500

    Initial Commit

---
+++ Notes/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  Notes
+//
+//  Created by Alex Hsieh on 11/2/12.
+//  Copyright (c) 2012 Alex Hsieh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+
+@property (strong, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
